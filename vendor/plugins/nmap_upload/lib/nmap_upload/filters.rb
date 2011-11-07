cd163e9771471414c519f7bffce5074145e81ff5
William Hall <williamjhall3@gmail.com>
2011-11-06 19:06:17 -0500

    nmap plugin: only hosts that are up and have open ports are added as notes. added script output to host note.

--- vendor/plugins/nmap_upload/lib/nmap_upload/filters.rb
+++ vendor/plugins/nmap_upload/lib/nmap_upload/filters.rb
@@ -38,52 +38,62 @@ module NmapUpload
 
 
     # TODO: do something with the Nmap::Parser::Session information
-    
+
     port_notes_to_add = {}
 
-    parser.hosts do |host|
-      host_label = host.addr
-      host_label = "#{host_label} (#{host.hostname})" if host.hostname
-      host_node = parent.children.find_or_create_by_label_and_type_id( host_label, Node::Types::HOST )
+    parser.hosts("up") do |host|
 
-      # add the nmap output for the host as notes to the node
-      host_info = "#{host.addr}:\n"
-      host_info << "\tHostnames: #{host.hostnames}\n"
-      host_info << "\tPort info:\n"
+      #only add host note if there are open ports
+      ports = host.getports(:any,"open")
 
-      port_hash = {}
-	    host.getports(:any) do |port|
-        port_info = ''
-        srv = port.service
-        port_info << "\t\tPort ##{port.num}/#{port.proto} is #{port.state} (#{port.reason})\n"
-        port_info << "\t\t\tService: #{srv.name}\n" if srv.name
-        port_info << "\t\t\tProduct: #{srv.product}\n" if srv.product
-        port_info << "\t\t\tVersion: #{srv.version}\n" if srv.version
-        port_info << "\n"
+      if ports.length > 0 then
+        host_label = host.addr
+        host_label = "#{host_label} (#{host.hostname})" if host.hostname
+        host_node = parent.children.find_or_create_by_label_and_type_id( host_label, Node::Types::HOST )
 
-        port_hash[ "#{port.num}/#{port.proto}" ] = port_info
-        host_info << port_info
-  		end
+        # add the nmap output for the host as notes to the node
+        host_info = "h1. #{host.addr}\n\n"
+        host_info << "Hostnames: #{host.hostnames}\n" if host.hostnames.length > 2
+
+        port_hash = {}
+        host.getports(:any) do |port|
+          port_info = ''
+          srv = port.service
+          port_info << "Port ##{port.num}/#{port.proto} is #{port.state} (#{port.reason})\n"
+          port_info << "Service: #{srv.name}\n" if srv.name
+          port_info << "Product: #{srv.product}\n" if srv.product
+          port_info << "Version: #{srv.version}\n" if srv.version
+          port_info << "\nScript Results:\n" if port.scripts.length > 0
+          port.scripts do |script|
+            port_info << "#{script.id}: #{script.output}\n\n"
+          end
+          port_info << "________________________________________________________________________"
+          port_info << "\n\n\n"
+
+          port_hash[ "#{port.num}/#{port.proto}" ] = port_info
+          host_info << port_info
+        end
 
 
-      Note.new(
-        :node_id => host_node.id,
-        :author => Configuration.author,
-        :category_id => category.id,
-        :text => host_info
-      ).save
-
-      port_hash.each do |port_name, info|
-        # Add a node for the port
-        port_node = host_node.children.find_or_create_by_label( port_name )
-
-        # add a note with the port information
         Note.new(
-          :node_id => port_node.id,
-          :author => 'Nmap',
+          :node_id => host_node.id,
+          :author => Configuration.author,
           :category_id => category.id,
-          :text => info
+          :text => host_info
         ).save
+
+        port_hash.each do |port_name, info|
+          # Add a node for the port
+          port_node = host_node.children.find_or_create_by_label( port_name )
+
+          # add a note with the port information
+          Note.new(
+            :node_id => port_node.id,
+            :author => 'Nmap',
+            :category_id => category.id,
+          :text => info
+          ).save
+        end
       end
     end
 
